cc109b7a2cd4790a13afe4febca0d7adef6061fd
Jiri Marsik <jiri.marsik89@gmail.com>
2012-03-21 20:59:56 +0800

    Initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject deflaw "1.0.0-SNAPSHOT"
+  :description "An exploration into expressing laws of interactions in a population using logic programming."
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/core.logic "0.6.7"]
+                 [seesaw "1.4.0"]])
---
+++ test/deflaw/test/core.clj
@@ -0,0 +1,6 @@
+(ns deflaw.test.core
+  (:use [deflaw.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
