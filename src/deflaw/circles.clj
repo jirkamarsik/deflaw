0bf5ccbe0528c129265f38251a93c615b712d0cf
Jiri Marsik <jiri.marsik89@gmail.com>
2012-03-23 01:30:24 +0800

    Circles which consume other circles

--- src/deflaw/circles.clj
+++ src/deflaw/circles.clj
@@ -13,16 +13,22 @@
 (defn init-pop [width height]
   (apply merge
          (repeatedly (rand 10)
-                     #(let [type (pick [::loner ::seeker])]
+                     #(let [type (pick [::loner ::seeker ::consumer])]
                         {(gensym (name type))
                          {:type   type
                           :x      (rand width)
                           :y      (rand height)
-                          :r      (+ 10 (rand 40))
-                          :color  (color (int (+ 56 (rand 200)))
-                                         (int (+ 56 (rand 200)))
-                                         (int (+ 56 (rand 200))))
-                          :stroke (+ 2 (rand 4))}}))))
+                          :r      (if (= type ::consumer)
+                                    (+ 30 (rand 60))
+                                    (+ 10 (rand 40)))
+                          :color  (if (= type ::consumer)
+                                    :red
+                                    (color (+ 56 (rand-int 200))
+                                           (+ 56 (rand-int 200))
+                                           (+ 56 (rand-int 200))))
+                          :stroke (if (= type ::consumer)
+                                    10
+                                    (+ 2 (rand 4)))}}))))
 
 (defn reset-world! []
   (swap! world (fn [{:keys [width height]}]
@@ -44,13 +50,16 @@
 (defmethod self-esteem ::circle [state id]
   (rand))
 
+(defn distance2 [x1 y1 x2 y2]
+  (apply + (map #(* % %) [(- x2 x1) (- y2 y1)])))
+
 (defn distances [ents id]
   (letfn [(distance-from [other-id]
             (let [x1 (get-in ents [id       :x])
                   x2 (get-in ents [other-id :x])
                   y1 (get-in ents [id       :y])
                   y2 (get-in ents [other-id :y])]
-              (apply + (map #(* % %) [(- x2 x1) (- y2 y1)]))))]
+              (distance2 x1 y1 x2 y2)))]
     (map distance-from (keys (dissoc ents id)))))
 
 (derive ::loner ::circle)
@@ -60,6 +69,10 @@
 (derive ::seeker ::circle)
 (defmethod self-esteem ::seeker [state id]
   (- (apply min (distances (:entities state) id))))
+
+(derive ::consumer ::circle)
+(defmethod self-esteem ::consumer [state id]
+  (get-in state [:entities id :r]))
 
 (defn in-boundso [state id]
   (fresh [x y w h]
@@ -72,13 +85,38 @@
     (la/<= 0 y)
     (la/<= y h)))
 
+(defn toucho [state id1 id2]
+  (fresh [e1 x1 y1 r1 e2 x2 y2 r2]
+    (get-ino state [:entities id1] e1)
+    (get-ino state [:entities id2] e2)
+    (geto e1 :x x1)
+    (geto e1 :y y1)
+    (geto e1 :r r1)
+    (geto e2 :x x2)
+    (geto e2 :y y2)
+    (geto e2 :r r2)
+    (project [x1 y1 r1 x2 y2 r2]
+      (la/<= (distance2 x1 y1 x2 y2) (* (+ r1 r2) (+ r1 r2))))))
+
 (defn possibleo [old id new]
-  (fresh [dim dir speed]
-   (membero dim [:x :y])
-   (membero dir [+ -])
-   (get-ino old [:entities id :stroke] speed)
-   (update-ino old [:entities id dim] new dir speed)
-   (in-boundso new id)))
+  (conde
+    [(get-ino old [:entities id :type] ::consumer)
+     (fresh [r oid or inter]
+       (get-ino old [:entities  id :r]  r)
+       (get-ino old [:entities oid :r] or)
+       (!= id oid)
+       (la/> r or)
+       (la/> or 0)
+       (toucho old id oid)
+       (update-ino old   [:entities  id :r] inter inc)
+       (update-ino inter [:entities oid :r] new   dec))]
+    [(fresh [type dim dir speed]
+       (get-ino old [:entities id :type] type)
+       (membero dim [:x :y])
+       (membero dir [+ -])
+       (get-ino old [:entities id :stroke] speed)
+       (update-ino old [:entities id dim] new dir speed)
+       (in-boundso new id))]))
 
 (defn act-entity [old-state id]
   (apply max-key #(self-esteem % id)
