5e53d856283f5223f05ddc937d59f89bf507dfb0
Jiri Marsik <jiri.marsik89@gmail.com>
2012-03-22 23:15:45 +0800

    The basic experiment setup

--- src/deflaw/core.clj
+++ src/deflaw/core.clj
@@ -1,1 +1,33 @@
-(ns deflaw.core)
+(ns deflaw.core
+  (:use seesaw.core
+        deflaw.circles))
+
+(native!)
+
+(defn draw-world [c g]
+  (doseq [ent (vals (:entities @world))]
+    (draw-entity g ent)))
+
+(defn update-world []
+  (doseq [id (keys (:entities @world))]
+    (swap! world act-entity id)))
+
+(defn canvas-resized [e]
+  (swap! world assoc :width (.getWidth (to-widget e)))
+  (swap! world assoc :height (.getHeight (to-widget e))))
+
+(defonce my-canvas (canvas :background :black
+                           :paint      draw-world
+                           :listen     [:component-resized canvas-resized]))
+
+(defonce my-frame (frame :title "deflaw" :content my-canvas))
+
+(show! my-frame)
+(canvas-resized my-canvas)
+
+(defonce my-timer (timer (fn [time]
+                           (update-world)
+                           (.repaint my-canvas)
+                           (inc time))
+                         :initial-value 0
+                         :delay 40))
